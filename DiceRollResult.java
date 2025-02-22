20ae4539472ca3e892c8f699cb85651415ac230b
Alexis94joel <99000336+Alexis94joel@users.noreply.github.com>
2025-02-21 22:31:09 -0600

    Update DiceRollResult.java

--- DiceRollResult.java
+++ DiceRollResult.java
@@ -1,4 +1,4 @@
         public static void main(String[] args) {
                 int[] dice = rollDice();
                 System.out.println("You rolled: " + dice[0] + " and " + dice[1] + " (Total: " + (dice[0] + dice[1]) + ")");
-        }
+        } 
